d834128a22457b264c733b69ab005dc672ed5068
Shreyas Kalyanaraman <shreyas.kalyanaraman@gmail.com>
2018-09-27 23:46:03 -0400

    Removing

--- Cloud Based Admission Process System/App_Data/dbo.Table.sql
+++
@@ -1,8 +0,0 @@
-﻿CREATE TABLE [dbo].[Table]
-(
-	[Id] INT NOT NULL PRIMARY KEY, 
-    [Name] NVARCHAR(MAX) NULL, 
-    [City] NVARCHAR(50) NULL, 
-    [State] NVARCHAR(MAX) NULL, 
-    [Zip] INT NULL
-)
